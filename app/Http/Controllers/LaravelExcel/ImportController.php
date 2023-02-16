46d87d0b3a6cf8eda71830c7d524400748f2bc03
SaaberDev <saaberdev@gmail.com>
2023-02-16 05:04:38 +0100

    see batch in array

--- app/Http/Controllers/LaravelExcel/ImportController.php
+++ app/Http/Controllers/LaravelExcel/ImportController.php
@@ -68,7 +68,8 @@ class ImportController extends Controller
 
     public function seeBatch($id)
     {
-        return \Bus::findBatch($id)->toArray();
+        echo 'Here is the array output of export batch';
+        dd(\Bus::findBatch($id)->toArray());
     }
 
     /**
